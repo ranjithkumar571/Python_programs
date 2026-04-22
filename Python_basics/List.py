edc26e1e0c41c1996ef6c531a3283f25633873d8
ranjith kumar m <ranjithkumar.mitt571@gmail.com>
2026-04-21 20:34:39 -0400

    List functionality

---
+++ Python_basics/List.py
@@ -0,0 +1,10 @@
+colors = ["Red", "Blue", "Green"]
+## Add 
+colors.append("White")
+print(colors)
+## insert 
+colors.insert(2,"Black")
+print(colors)
+## remove
+colors.remove("Blue")
+print(colors)
